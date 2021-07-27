d65c7fe1a6e79ca05eb39a20810e6a8b5dd762fd
Hugo Gallardo <hugoeduardogallardo@gmail.com>
2021-07-27 15:38:39 -0300

    inicializacion

--- application/config/constants.php
+++ application/config/constants.php
@@ -135,6 +135,9 @@ define('viewOT', false);
 #COMPONENTE SICPOA
 define('SICP', 'ddpe-tools-pro/');
 
+#COMPONENTE SICPOA
+define('COD', 'traz-comp-codigos/');
+
 define('BPM_PROCESS', json_encode(array(
     BPM_PROCESS_ID_REPARACION_NEUMATICOS => ['nombre' => 'Proceso de Reparación de Neumáticos', 'color' => '#0275d8', 'proyecto'=>YUDIPROC, 'model'=>'Yudiproctareas'],
     BPM_PROCESS_ID_PEDIDOS_NORMALES => ['nombre' => 'Ped. Materiales', 'color' => '#F39C12', 'proyecto'=>ALM, 'model'=>'Almtareas'],
