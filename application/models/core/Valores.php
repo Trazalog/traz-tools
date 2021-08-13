ff1be5ae04a530fbd22705c5b324f2bc45ceb10f
Gerardo Ramos <geramos55@gmail.com>
2021-08-13 13:02:50 -0300

    Subida backup

--- application/models/core/Valores.php
+++ application/models/core/Valores.php
@@ -15,22 +15,9 @@ class Valores extends CI_Model {
   }
 
   /**
-  * Genera un valore nuevo
-  * @param array con datos de Cliente
-  * @return ID valor generado
-  */
-  function guardarValor($data){
-    $post['_post_cliente'] = $data;
-    log_message('DEBUG','#TRAZA| TRAZ-TOOLS | LISTA DE VALORES | guardarValor()  $post: >> '.json_encode($post));
-    $aux = $this->rest->callAPI("POST",REST_CORE."/tablas", $post);
-    $aux = json_decode($aux["data"]);
-    return $aux->GeneratedKeys->Entry;
-  }
-
-  /**
   * Listado de Valores
   * @param 
-  * @return array con datos de valores
+  * @return array con listado de valores
   */
   function Listar_Valores(){
     $empr_id = empresa();
@@ -41,11 +28,13 @@ class Valores extends CI_Model {
   }
 
   /**
-  * Contenido de un Valor
-  * @param 
+  * Obtener contenido de un Valor
+  * @param valor de tabla
   * @return array con datos del valor
   */
   function getValor($tabla){
+    $post['_post_valor'] = $tabla;
+    log_message('DEBUG','#TRAZA| TRAZ-TOOLS | VALORES | getValor() $post: >> '.json_encode($post));
     $empre_id = empresa();
     $aux = $this->rest->callAPI("GET",REST_CORE."/tabla/".$tabla."/empresa/",$empre_id);
     $aux = json_decode($aux["data"]);
@@ -54,40 +43,27 @@ class Valores extends CI_Model {
   }
 
   /**
-  * Listado tipos de Valores
-  * @param 
-  * @return array con datos de valores
+  * Genera un valor nuevo
+  * @param array con datos del valor
+  * @return ID valor generado
   */
-  function Listar_Tipos_Valores(){
-		$aux = $this->rest->callAPI("GET",REST_CORE."/tablas/tipos_valores");
-		$aux = json_decode($aux["data"]);
-		$tipos_valores = $aux->tablas->tabla;
-		return $tipos_valores;
+  function guardarValor($data){
+    $post['_post_valor'] = $data;
+    log_message('DEBUG','#TRAZA| TRAZ-TOOLS | VALORES | guardarValor()  $post: >> '.json_encode($post));
+    $aux = $this->rest->callAPI("POST",REST_CORE."/tablas", $post);
+    $aux = json_decode($aux["data"]);
+    return $aux->GeneratedKeys->Entry;
   }
   
   /**
-   * Edita info del Cliente
-   * @param array con info
-   * @return bool respuesta de servicio
-   */
-  function Editar_Cliente($cliente)
-  {
-    $post['_put_cliente'] = $cliente;
-    log_message('DEBUG','#TRAZA | TRAZ-TOOLS | VALORES | Editar_Cliente() $post: >> '.json_encode($post));
-    $aux = $this->rest->callAPI("PUT",REST_CORE."/cliente", $post);
-    $aux = json_decode($aux["status"]);
-    return $aux;
-  }
-  
-  /**
-    * Borra tabla por ID
+    * Borrado lógico de valor por ID
     * @param	int $tabl_id
     * @return bool true o false resultado del servicio
     */
     function borrarValor($tabl_id)
     {
-      $post['_delete_tabla'] = array("tabl_id" => $tabl_id);
-      log_message('DEBUG','#TRAZA | TRAZ-TOOLS | VALORES $post: >> '.json_encode($post));
+      $post['_delete_valor'] = array("tabl_id" => $tabl_id);
+      log_message('DEBUG','#TRAZA | TRAZ-TOOLS | VALORES | borrarValor() $post: >> '.json_encode($post));
       $aux = $this->rest->callAPI("DELETE",REST_CORE."/tabla", $post);
       $aux = json_decode($aux["status"]);
       return $aux;
