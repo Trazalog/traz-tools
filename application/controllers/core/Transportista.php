3a9a367e2293ee3d96915bd76ea6c2370c42fd8e
kevin marchan <kevinmichael.m@outlook.es>
2022-10-11 16:18:43 -0300

    ABM Transportistas

--- application/controllers/core/Transportista.php
+++ application/controllers/core/Transportista.php
@@ -17,8 +17,49 @@ class Transportista extends CI_Controller
 	{
 		log_message('INFO','#TRAZA | TRANSPORTISTAS | index()  >> ');
 		$data['tipos_transportistas'] = $this->Transportistas->Listar_Tipos_Transportistas();
+		$data['tipos_transporte'] = $this->Transportistas->getTiposTransporte();
+		$data['listarPaises'] = $this->Transportistas->listarPaises();
 		$this->load->view('core/transportistas/view', $data);
     }
+
+	/**
+	* Devuelve un listado de estados dependiendo del id de pais que le enviemos.
+	*
+	* @return  Array   Devuelve un arreglo con estados.
+	*/
+	public function getEstados()
+	{
+		log_message('INFO','#TRAZA| TRANSPORTISTAS | getEstados() >> ');
+		$pais = $this->input->post('id_pais');
+		$resp = $this->Transportistas->getEstados($pais);
+		echo json_encode($resp);
+	}
+
+	/**
+	* Devuelve un listado de localidades dependiendo del id de estado que le enviemos.
+	*
+	* @return  Array   Devuelve un arreglo con localidades.
+	*/
+	public function getLocalidades()
+	{
+		log_message('INFO','#TRAZA| TRANSPORTISTAS | getLocalidades() >> ');
+		$pais = $this->input->post('id_pais');
+		$estado = $this->input->post('id_estado');
+		$resp = $this->Transportistas->getLocalidades($pais, $estado);
+		echo json_encode($resp);
+	}
+
+	/**
+	* Devuelve un listado de tipos de transporte.
+	*
+	* @return  Array   Devuelve un arreglo con tipos de transporte.
+	*/
+	// public function getTiposTransporte()
+	// {
+	// 	log_message('INFO','#TRAZA| TRANSPORTISTAS | getTiposTransporte() >> ');
+	// 	$resp = $this->Transportistas->getTiposTransporte();
+	// 	echo json_encode($resp);
+	// }
     
  	/**
 	* Devuelve un listado de las Transportistas.
@@ -39,17 +80,23 @@ class Transportista extends CI_Controller
 	*/
 	function Guardar_Transportista()
 	{
-		$transportista = $this->input->post('datos');
-		$transportista['usuario_app'] = userNick();
-		$transportista['empr_id'] = empresa();
-		$resp = $this->Transportistas->Guardar_Transportista($transportista);
+		$data = $this->input->post('datos');
+		$pais_id = urlencode($data['pais_id']);
+		$prov_id = urldecode($data['prov_id']);
+		$loca_id = urldecode($data['loca_id']);
+		$data['empr_id'] = empresa();
+		$data['pais_id'] = $pais_id;
+		$data['prov_id'] = $prov_id;
+		$data['loca_id'] = $loca_id;
+		$data['tipo_transporte'] = urldecode($data['tipo_transporte']);
+		$resp = $this->Transportistas->Guardar_Transportista($data);
+		if ($resp['status']) {
+			$resp['message'] = "Se agregó el transportista correctamente";
+		}else{
+			$resp['message'] = "Se produjo un error al guardar el transportista";
+		}
 		echo json_encode($resp);
-    	log_message('ERROR', '#TRAZA | TRANSPORTISTAS | guardar() >> $transportista_id: '.$transportista);
-		// if ($resp != null) {
-		// 	return json_encode(true);
-		// } else {
-		// 	return json_encode(false);
-		// }
+    	log_message('ERROR', '#TRAZA | TRANSPORTISTAS | guardar() >> $transportista_id: '.$data);
 	}
 
 	/**
@@ -60,10 +107,23 @@ class Transportista extends CI_Controller
 	function Editar_Transportista()
 	{
     	log_message('INFO','#TRAZA | TRANSPORTISTAS | Editar_Transportista() >> ');
-		$transportista = $this->input->post('datos');
-		$transportista['usuario_app'] = userNick();
-		$resp = $this->Transportistas->Editar_Transportista($transportista);
+		$data = $this->input->post('datos');
+		$pais_id = urlencode($data['pais_id']);
+		$prov_id = urldecode($data['prov_id']);
+		$loca_id = urldecode($data['loca_id']);
+		$data['empr_id'] = empresa();
+		$data['pais_id'] = $pais_id;
+		$data['prov_id'] = $prov_id;
+		$data['loca_id'] = $loca_id;
+		$data['tipo_transporte'] = urldecode($data['tipo_transporte']);
+		$resp = $this->Transportistas->Editar_Transportista($data);
+		if ($resp['status']) {
+			$resp['message'] = "Se editó el transportista correctamente";
+		}else{
+			$resp['message'] = "Se produjo un error al editar el transportista";
+		}
 		echo json_encode($resp);
+    	log_message('ERROR', '#TRAZA | TRANSPORTISTAS | guardar() >> $transportista_id: '.$data);
 	}
 
 	/**
@@ -74,8 +134,8 @@ class Transportista extends CI_Controller
 	public function Borrar_Transportista()
 	{
 		log_message('INFO','#TRAZA | TRANSPORTISTAS | Borrar_Transportista() >> ');
-		$cuit = $this->input->post('cuit');
-		$result = $this->Transportistas->Borrar_Transportista($cuit);
+		$tran_id = $this->input->post('tran_id');
+		$result = $this->Transportistas->Borrar_Transportista($tran_id);
 		echo json_encode($result);
 	}
 }    
--- application/models/core/Transportistas.php
+++ application/models/core/Transportistas.php
@@ -23,8 +23,8 @@ class Transportistas extends CI_Model {
     $post['_post_transportista'] = $data;
     log_message('DEBUG','#TRAZA| TRAZ-TOOLS | TRANSPORTISTAS | Guardar_Transportista()  $post: >> '.json_encode($post));
     $aux = $this->rest->callAPI("POST",REST_CORE."/transportista", $post);
-    $aux = json_decode($aux["data"]);
-    return $aux->GeneratedKeys->Entry;
+    // $aux = json_decode($aux["data"]);
+    return $aux;
   }
 
   /**
@@ -41,6 +41,67 @@ class Transportistas extends CI_Model {
   }
 
   /**
+   * Obtener listado de paises
+  * @return array con listado de paises
+  */
+  function listarPaises() {
+    $resource = "/paises";
+    $url = REST_CORE . $resource;
+    $aux = $this->rest->callApi('GET', $url);
+    $aux = json_decode($aux["data"]);
+    $paises = $aux->paises->pais;
+    return $paises;
+  }
+
+  /**
+   * Obtener estados dependiendo el pais seleccionado
+  * @param valor de pais
+  * @return array con listado de estados
+  */
+  function getEstados($pais) {
+      $post['_post_valor'] = $pais;
+      log_message('DEBUG','#TRAZA| TRAZ-TOOLS | TRANSPORTISTAS | getEstados() $post: >> '.json_encode($post));
+      $pais = urlencode($pais);
+      $resource = "/estados/pais/".$pais;
+      $url = REST_CORE . $resource;
+      $aux = $this->rest->callApi('GET', $url); 
+      $aux = json_decode($aux["data"]);
+      $valores = $aux->estados->estado;
+      return $valores;
+  }
+
+  /**
+   * Obtener localidades dependiendo del pais y estado seleccionado
+  * @param valor de pais y estado
+  * @return array con listado de localidades
+  */
+  function getLocalidades($pais, $estado) {
+      $post['_post_pais'] = $pais;
+      $post['_post_estado'] = $estado;
+      log_message('DEBUG','#TRAZA| TRAZ-TOOLS | TRANSPORTISTAS | getLocalidades() $post: >> '.json_encode($post));
+      $pais = urlencode($pais);
+      $estado = urlencode($estado);
+      $resource = '/localidades/pais/' . $pais . '/estado/' . $estado;
+      $url = REST_CORE . $resource;
+      $aux = $this->rest->callApi('GET', $url); 
+      $aux = json_decode($aux["data"]);
+      $valores = $aux->localidades->localidad;
+      return $valores;
+  }
+
+  /**
+  * Listado tipos de transporte
+  * @param 
+  * @return array con datos de tipos de transporte
+  */
+  function getTiposTransporte(){
+    $aux = $this->rest->callAPI("GET",REST_CORE."/tablas/tipos_transporte");
+		$aux = json_decode($aux["data"]);
+		$tipos_transporte = $aux->tablas->tabla;
+		return $tipos_transporte;
+  }
+
+  /**
   * Listado tipos de Transportistas
   * @param 
   * @return array con datos de transportistas
@@ -54,31 +115,30 @@ class Transportistas extends CI_Model {
   }
   
   /**
-   * Edita info del Transportista
-   * @param array con info
-   * @return bool respuesta de servicio
-   */
+  * Edita info del Transportista
+  * @param array con info
+  * @return bool respuesta de servicio
+  */
   function Editar_Transportista($transportista)
   {
     $post['_put_transportista'] = $transportista;
     log_message('DEBUG','#TRAZA | TRAZ-TOOLS | TRANSPORTISTAS | Editar_Transportista() $post: >> '.json_encode($post));
     $aux = $this->rest->callAPI("PUT",REST_CORE."/transportista", $post);
-    $aux = json_decode($aux["status"]);
+    // $aux = json_decode($aux["status"]);
     return $aux;
   }
   
   /**
-    * Borra transportista por ID
-    * @param	int $clie_id
-    * @return bool true o false resultado del servicio
-    */
-    function Borrar_Transportista($clie_id)
-    {
-      $post['_delete_transportista'] = array("clie_id" => $clie_id);
-      log_message('DEBUG','#TRAZA | TRAZ-TOOLS | TRANSPORTISTAS $post: >> '.json_encode($post));
-      $aux = $this->rest->callAPI("DELETE",REST_CORE."/transportista", $post);
-      $aux = json_decode($aux["status"]);
-      return $aux;
-    }
+  * Borra transportista por ID
+  * @param	int $tran_id
+  * @return bool true o false resultado del servicio
+  */
+  function Borrar_Transportista($tran_id)
+  {
+    $post['_delete_transportista'] = array("tran_id" => $tran_id);
+    log_message('DEBUG','#TRAZA | TRAZ-TOOLS | TRANSPORTISTAS $post: >> '.json_encode($post));
+    $aux = $this->rest->callAPI("DELETE",REST_CORE."/transportista", $post);
+    return $aux;
+  }
 
 }
