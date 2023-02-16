cf0bd02611f2341a49076aa4ae8910e7ad506629
focafunk <ruiz.rodolfo@gmail.com>
2023-02-16 17:47:08 -0300

    Prueba exitosa en Windows
    Lectura sin Reader, evitando el \n, y leyendo solo los databits del InputStream

--- _backend/api/BasculaConnector/org.wso2.carbon.esb.connector.tools.bascula/src/main/java/org/wso2/carbon/connector/BasculaConnector.java
+++ _backend/api/BasculaConnector/org.wso2.carbon.esb.connector.tools.bascula/src/main/java/org/wso2/carbon/connector/BasculaConnector.java
@@ -1,21 +1,4 @@
 package org.wso2.carbon.connector;
-/*
-*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
 
 import com.fazecast.jSerialComm.*; 
 import org.apache.commons.logging.Log;
@@ -23,19 +6,23 @@ import org.apache.synapse.MessageContext;
 import org.wso2.carbon.connector.core.AbstractConnector;
 import org.wso2.carbon.connector.core.ConnectException;
 
-import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Enumeration;
 
-/**
- * Sample method implementation.
- */
+
+/* Conector para leer el peso de una báscula vía port serial COM
+ * @author rruiz - TZL
+*/
 public class BasculaConnector extends AbstractConnector {
 
 	private Log log;
 	private SerialPort port = null;
-
+	String portName;
+	String bauds;
+	String databits;
+	String stopbits;
+	String parity;
 
 	/**
 	 * Abre una instancia unica de puerto para recibir el peso
@@ -44,11 +31,11 @@ public class BasculaConnector extends AbstractConnector {
 	 */
 	public void openPort(MessageContext mc) throws Exception {
 	try{
-			String portName = (String) getParameter(mc, "portname");
-			String bauds = (String) getParameter(mc, "bauds");
-			String databits = (String) getParameter(mc, "databits");
-			String stopbits = (String) getParameter(mc, "stopbits");
-			String parity = (String) getParameter(mc, "parity");
+			portName = (String) getParameter(mc, "portname");
+			bauds = (String) getParameter(mc, "bauds");
+			databits = (String) getParameter(mc, "databits");
+			stopbits = (String) getParameter(mc, "stopbits");
+			parity = (String) getParameter(mc, "parity");
 
 			try{
 
@@ -60,7 +47,7 @@ public class BasculaConnector extends AbstractConnector {
 				throw sie;	
 			}
 
-			port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
+			port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
 
 			log.info("BASCCONN: abriendo puerto "+portName);
 			if (port.openPort()){
@@ -80,14 +67,14 @@ public class BasculaConnector extends AbstractConnector {
 	}
 
 	/**
-	 * Abre un InputStream esperando la trama que la báscula envia por el puerto en serie portName
+	 * Abre un InputStream esperando la trama que la báscula envia por el puerto en serie portName, lee solo la cantidad de databits
 	 * @param mc wso2 message conext
 	 */
 	@Override
 	public void connect(MessageContext mc) throws ConnectException {
+		InputStream is;
 		try {
 			log = mc.getServiceLog();
-			BufferedReader is;
 			
 			log.info("BASCCONN: connect");
 			//Si es la primer vez que nos conectamos, crea la conexión para el puerto
@@ -104,20 +91,20 @@ public class BasculaConnector extends AbstractConnector {
 			try {
 				//Leo el peso de la bascula
 				log.info("BASCCONN: leyendo peso");
-				is = new BufferedReader(new InputStreamReader(port.getInputStream()));
-
-				// obtengo el string recibido con el peso y lo seteo en la propiedad de wso2
+				is= port.getInputStream();
+				log.info("BASCCONN: leyendo peso");
 
 				String trama = "";
-				if (is != null) {
-					log.info("BASCCONN: readLine()");
-					trama = is.readLine();
-					log.info("BASCCONN: peso leido" + trama);					
-					mc.setProperty("pesoBascula", trama);
-					log.info("BASCCONN: pesoBascula seteada" + trama);					
-					
+								//br = new BufferedReader(new InputStreamReader(port.getInputStream()));
+				char c; 
+				for (int cant=0 ; cant<Integer.parseInt(databits) ; cant++){
+					c = (char) is.read(); 
+					log.info("BASCCONN: c "+c);
+					trama += c + ""; 
 				}
 
+				log.info("BASCCONN: peso leido" + trama);					
+				mc.setProperty("pesoBascula", trama);
 				log.info("BASCCONN: Set property a mc, pesoBascula" + " = " + trama);
 			} catch (SerialPortTimeoutException spte) {
 				log.info("BASCCONN: tmout ");
@@ -130,6 +117,7 @@ public class BasculaConnector extends AbstractConnector {
 			// Finalizo la lectura
 			try {
 				log.info("BASCCONN: cierro lectura");
+				//br.close();
 				is.close();
 				log.info("BASCCONN: cerrado");
 			} catch (Exception e) {
