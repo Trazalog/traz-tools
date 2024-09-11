b5f83dc6b68ec5b91b9eb3a5226cb13b9fc25fb1
Gera Ramos <geramos55@gmail.com>
2024-09-10 22:20:51 -0300

    ABM lista de Precios y versiones
    ejecutar script comentado en ticket para crear item del menu

---
+++ application/views/core/precios/list.php
@@ -0,0 +1,19 @@
+
+<!-- ______ TABLA PRINCIPAL DE PANTALLA ______ -->
+<p>holis</p>
+<table id="tabla_precios" class="table table-bordered table-striped">
+		<thead class="thead-dark" bgcolor="#eeeeee">
+      <th>Acciones</th>
+      <th>Id</th>
+      <th>Descripción</th>
+      <th>Área</th>
+      <th>Código</th>
+      <th>Sector</th>
+      <th>Criticidad</th>
+      <th>Estado</th>
+		</thead>
+		<tbody >
+			
+		</tbody>
+</table>
+<!--_______ FIN TABLA PRINCIPAL DE PANTALLA ______-->
